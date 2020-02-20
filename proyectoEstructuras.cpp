56223af881d1e7ae6628cb0246567c71c7ffe83c
richfon <richfonpan@gmail.com>
2020-02-20 00:12:46 -0500

    validates help commands

--- proyectoEstructuras.cpp
+++ proyectoEstructuras.cpp
@@ -19,77 +19,77 @@ int main(int argc, char *argv[])
     /*------------------------Richard Fonseca--------------------------*/
     //CODE
     //los ayudas
-    elseif(strcmp(comando, ayuda) == 0)
+    elseif(strcmp(comando, "ayuda") == 0)
     {
         cout << "AYUDA \n";
         cout<<"Comandos disponibles: \n"
-        /*<<"cargar \t"<<"conteo \t"<<"listar_secuencia \t"<<"histograma \n"
+        <<"cargar \t"<<"conteo \t"<<"listar_secuencia \t"<<"histograma \n"
         <<"es_subsecuencia \t"<<"enmascarar \t"<<"guardar \t"<<"salir \n"
         <<"codificar \t"<<"decodificar \t"<<"ruta_mas_corta \t"<<"base_remota \n";
 
-        cout<<"Ademas se incluyen los comandos ayuda de cada comando, ayuda_nombre_del_comando \n";
+        cout<<"Se incluyen los comandos ayuda de cada comando, ayuda_nombre_del_comando \n";
 
         cout<<"ayuda_cargar \t"<<"ayuda_conteo \t"<<"ayuda_listar_secuencia \t"<<"ayuda_histograma \n"
-        <<"ayuda_es_subsecuencia \t"<<"ayuda_enmascarar \t"<<"ayuda_guardar \t"<<"ayuda_salir \n"//es_subsecuencia \t
-        <<"ayuda_codificar \t"<<"ayuda_decodificar \t"<<"ayuda_ruta_mas_corta\t"<<"ayuda_base_remota\n";*/
+        <<"ayuda_es_subsecuencia \t"<<"ayuda_enmascarar \t"<<"ayuda_guardar \t"<<"ayuda_salir \n"
+        <<"ayuda_codificar \t"<<"ayuda_decodificar \t"<<"ayuda_ruta_mas_corta\t"<<"ayuda_base_remota\n";
     }
-    elseif(strcmp(comando, ayuda_cargar) == 0)
+    elseif(strcmp(comando, "ayuda_cargar") == 0)
     {
         cout << "AYUDA_CARGAR \n";
         cout<<"la manera correcta de llamar al comando cargar es: "<<"cargar";
     }
-    elseif(strcmp(comando, ayuda_conteo) == 0)
+    elseif(strcmp(comando, "ayuda_conteo") == 0)
     {
         cout << "AYUDA_CONTEO \n";
     cout<<"la manera correcta de llamar al comando conteo es: "<<"conteo";
     }
-    elseif(strcmp(comando, ayuda_listar_secuencias) == 0)
+    elseif(strcmp(comando, "ayuda_listar_secuencias") == 0)
     {
         cout << "AYUDA_LISTAR_SECUENCIAS \n";
         cout<<"la manera correcta de llamar al comando listar secuencias es: "<<"listar_secuencia";
     }
-    elseif(strcmp(comando, ayuda_histograma) == 0)
+    elseif(strcmp(comando, "ayuda_histograma") == 0)
     {
         cout << "AYUDA_HISTOGRAMA \n";
         cout<<"la manera correcta de llamar al comando histograma es: "<<"histograma";
     }
     
-    elseif(strcmp(comando, ayuda_es_subsecuencia) == 0)
+    elseif(strcmp(comando, "ayuda_es_subsecuencia") == 0)
     {
         cout << "AYUDA_ES_SUBSECUENCIA \n";
         cout<<"la manera correcta de llamar al comando es subsecuencia es: "<<"es_subsecuencia";
     }
-    elseif(strcmp(comando, ayuda_enmascarar) == 0)
+    elseif(strcmp(comando, "ayuda_enmascarar") == 0)
     {
         cout << "AYUDA_ENMASCARAR \n";
         cout<<"la manera correcta de llamar al comando enmascarar es: "<<"enmascarar";
     }
-    elseif(strcmp(comando, ayuda_guardar) == 0)
+    elseif(strcmp(comando, "ayuda_guardar") == 0)
     {
         cout << "AYUDA_GUARDAR \n";
         cout<<"la manera correcta de llamar al comando guardar es: "<<"guardar";
     }
-    elseif(strcmp(comando, ayuda_salir) == 0)
+    elseif(strcmp(comando, "ayuda_salir") == 0)
     {
         cout << "AYUDA_SALIR \n";
         cout<<"la manera correcta de llamar al comando salir es: "<<"salir";
     }
-    elseif(strcmp(comando, ayuda_codificar) == 0)
+    elseif(strcmp(comando, "ayuda_codificar") == 0)
     {
         cout << "AYUDA_CODIFICAR \n";
         cout<<"la manera correcta de llamar al comando codificar es: "<<"codificar";
     }
-    elseif(strcmp(comando, ayuda_decodificar) == 0)
+    elseif(strcmp(comando, "ayuda_decodificar") == 0)
     {
         cout << "AYUDA_DECODIFICAR \n";
         cout<<"la manera correcta de llamar al comando decodificar es: "<<"decodificar";
     }
-    elseif(strcmp(comando, ayuda_ruta_mas_corta) == 0)
+    elseif(strcmp(comando, "ayuda_ruta_mas_corta") == 0)
     {
         cout << "AYUDA_RUTA_MAS_CORTA \n";
         cout<<"la manera correcta de llamar al comando ruta mas corta es: "<<"ruta_mas_corta";
     }
-    elseif(strcmp(comando, ayuda_base_remota) == 0)
+    elseif(strcmp(comando, "ayuda_base_remota") == 0)
     {
         cout << "AYUDA_BASE_REMOTA \n";
         cout<<"la manera correcta de llamar al comando base remota es: "<<"base_remota";
