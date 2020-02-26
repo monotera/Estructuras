46a36468e783ac812671da89c42372042b1bf703
monotera <nelson.a.mosquera@gmail.com>
2020-02-26 10:19:39 -0500

    Resolves 2 task with TAD

--- Taller-2/taller-2.cxx
+++ Taller-2/taller-2.cxx
@@ -5,19 +5,18 @@
 #include <list>
 #include <queue>
 #include <algorithm>
+#include "encontrar_palabras.h"
 
 using namespace std;
-
-void contiene(char *palabraBuscar, list<string> linea);
-void separateWords(char *str, queue<char *> &palabras);
-void comienza(char *palabraBuscar, list<string> linea);
 
 int main(int argc, char *argv[])
 {
     string line;
+    encontrar_palabras encontrar;
     ifstream myfile(argv[1]);
     char *palabraBuscar;
     list<string> linea;
+    
     if (myfile.is_open())
     {
         while (getline(myfile, line))
@@ -29,89 +28,17 @@ int main(int argc, char *argv[])
         palabraBuscar = new char[(*linea.begin()).size()];
         strcpy(palabraBuscar, (*linea.begin()).c_str());
         linea.pop_front();
-        comienza(palabraBuscar, linea);
-        contiene(palabraBuscar, linea);
+        encontrar.comienza(palabraBuscar,linea);
+        encontrar.contiene(palabraBuscar, linea);
         string rev(palabraBuscar);
         reverse(rev.begin(),rev.end());
         strcpy(palabraBuscar,rev.c_str());
-        contiene(palabraBuscar,linea);
+        encontrar.contiene(palabraBuscar,linea); 
+        
         myfile.close();
     }
     else
         cout << "Unable to open file";
     return 0;
-}
-
-void contiene(char *palabraBuscar, list<string> linea)
-{
-    cout << "--------------------------------------------------------" << endl;
-    list<string>::iterator it;
-    queue<char *> palabra;
-    char *str;
-    int cantiPal = 0, numLinea = 1;
-    for (it = linea.begin(); it != linea.end(); ++it)
-    {
-        char *str = new char[(*it).size()];
-        strcpy(str, (*it).c_str());
-        separateWords(str, palabra);
-        while (!palabra.empty())
-        {
-            if (strstr(palabra.front(), palabraBuscar) != nullptr)
-            {
-                cout << "Linea " << numLinea << ": " << palabra.front() << endl;
-                cantiPal++;
-            }
-            palabra.pop();
-        }
-        numLinea++;
-    }
-    cout << "Hay " << cantiPal << " palabras que contienen " << palabraBuscar << endl;
-}
-void comienza(char *palabraBuscar, list<string> linea)
-{
-    cout << "--------------------------------------------------------------" << endl;
-    list<string>::iterator it;
-    queue<char *> palabra;
-    char *str;
-    int cantiPal = 0, numLinea = 1;
-    bool bandera = true;
-    for (it = linea.begin(); it != linea.end(); ++it)
-    {
-        char *str = new char[(*it).size()];
-        strcpy(str, (*it).c_str());
-        separateWords(str, palabra);
-        while (!palabra.empty())
-        {
-            for (int i = 0; i < strlen(palabraBuscar); ++i)
-            {
-                if (palabraBuscar[i] != (palabra.front())[i])
-                {
-                    bandera = false;
-                }
-            }
-            if (bandera)
-            {
-                cout << "Linea " << numLinea << ": " << palabra.front() << palabra.front() << endl;
-                cantiPal++;
-            }
-            palabra.pop();
-            bandera = true;
-        }
-        numLinea++;
-    }
-    cout << "Hay " << cantiPal << " palabras que comienzan por " << palabraBuscar << endl;
-}
-
-void separateWords(char *str, queue<char *> &palabras)
-{
-    int cont = 0;
-    char *p;
-    p = strtok(str, " ,");
-    while (p != NULL)
-    {
-        palabras.push(p);
-        cont++;
-        p = strtok(NULL, " ,");
-    }
 }
 
