b2f7bdf75e03661e70d578a1acdd65421edd8830
monotera <nelson.a.mosquera@gmail.com>
2020-03-17 21:09:11 -0500

    Creates arboles directory

---
+++ arboles/nodoBinario.hxx
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "nodoBinario.h"
+
+using namespace std;
+
+
+/*template <class T>
+T& nodoBinario<T>::obtenerDato(){
+
+}*/
+template <class T>
+void nodoBinario<T>::fijarDato(T& val){
+
+}
+/*template <class T>
+nodoBinario<T>* nodoBinario<T>::obtenerHijoIzq(){
+
+}
+template <class T>
+nodoBinario<T>* nodoBinario<T>::obtenerHijoDer(){
+
+}*/
+template <class T>
+void nodoBinario<T>::fijarHijoIzq(nodoBinario<T> *izq){
+
+}
+template <class T>
+void nodoBinario<T>::fijarHijoDer(nodoBinario<T> *der){
+
+}
